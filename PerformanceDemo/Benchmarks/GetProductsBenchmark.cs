74c14057899f00dc4283d1e5b449e248d343adcf
Shamsiddin-it <samsiddinarbobov@gmail.com>
2026-03-13 15:18:43 +0500

    added perfomance testing with benchmarkdotnet

---
+++ PerformanceDemo/Benchmarks/GetProductsBenchmark.cs
@@ -0,0 +1,52 @@
+using BenchmarkDotNet.Attributes;
+using Microsoft.EntityFrameworkCore;
+using WebApi.DTOs;
+using WebApi.Entities;
+using WebApi.Services;
+
+namespace PerformanceDemo.Benchmarks;
+
+[MemoryDiagnoser]
+public class GetProductsBenchmark
+{
+    private ApplicationDbContext _context = null!;
+    private ProductService _service = null!;
+
+    [Params(100, 1_000, 10_000)]
+    public int ProductCount { get; set; }
+
+    [GlobalSetup]
+    public async Task Setup()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase($"products-benchmark-{ProductCount}")
+            .Options;
+
+        _context = new ApplicationDbContext(options);
+        _service = new ProductService(_context);
+
+        var products = Enumerable.Range(1, ProductCount)
+            .Select(index => new Product
+            {
+                Id = index,
+                Name = $"Product {index}",
+                Price = index * 10,
+                Description = $"Description for product {index}"
+            });
+
+        await _context.products.AddRangeAsync(products);
+        await _context.SaveChangesAsync();
+    }
+
+    [Benchmark]
+    public Task<List<ProductDto>> GetProducts()
+    {
+        return _service.GetProducts();
+    }
+
+    [GlobalCleanup]
+    public async Task Cleanup()
+    {
+        await _context.DisposeAsync();
+    }
+}
